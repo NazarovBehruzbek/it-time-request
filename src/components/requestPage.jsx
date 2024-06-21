2675ffa320cf2d768640102e4208580b3419fd37
Nazarov Behruzbek <behruzbeknazarov001@gmail.com>
2024-06-21 21:58:03 +0500

    solve

--- src/components/requestPage.jsx
+++ src/components/requestPage.jsx
@@ -55,7 +55,7 @@ function RequestPage() {
             setErrors({});
 
             const token = "7207834215:AAGpiV02gcPvk86_lLkfEoc9eC7TQuFoYZE";
-            const chat_id = -4253120413;
+            const chat_id = -1002201412210;
             const url = `https://api.telegram.org/bot${token}/sendMessage`;
             const messageContent = `${value==1?"#offline":"#online"} \nIsmi: ${name} \nUsername: ${username} \nTelefon: ${phone}`;
 
