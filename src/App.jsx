80f6c7760a53a606e00bddb3e61167d2c2731161
Nazarov Behruzbek <behruzbeknazarov001@gmail.com>
2024-11-09 13:03:04 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import RequestPage from './components/requestPage'
-import { ToastContainer } from 'react-toastify'
 function App() {
   return (
    <React.Fragment>
     <RequestPage/>
-    <ToastContainer/>
    </React.Fragment>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router.jsx';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <ToastContainer/>
   </React.StrictMode>
 );
