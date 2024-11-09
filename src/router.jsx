0eea6c26079ee02bacc7ffaf4975e3214ba45c99
Nazarov Behruzbek <behruzbeknazarov001@gmail.com>
2024-11-09 12:30:35 +0500

    add

--- src/router.jsx
+++ src/router.jsx
@@ -1,23 +1,24 @@
-import {  createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import Kiberxavfsizlik from './components/CyberSecurity';
 import Limsa from './components/App';
-import NewTravel from './components/app/NewTravel'
+import NewTravel from './components/app/NewTravel';
+
 export const router = createBrowserRouter([
-    {
-        path:'/',
-        element:<App/>
-    },
-    {
-        path:'/cyber',
-        element:<Kiberxavfsizlik/>
-    },
-    {
-        path:"/limsa",
-        element:<Limsa/>
-    },
-    {
-        path:"/travel",
-        element:<NewTravel/>
-    }
-])
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/cyber',
+    element: <Kiberxavfsizlik />,
+  },
+  {
+    path: '/limsa',
+    element: <Limsa />,
+  },
+  {
+    path: '/travel',
+    element: <NewTravel />,
+  },
+]);
